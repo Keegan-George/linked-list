aef7d9a95f7570785f6422d786d22f9172b7b7f6
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-08-06 20:23:49 -0400

    Update length of linked list for testing and include tests for at() and pop()

--- linked-list.test.js
+++ linked-list.test.js
@@ -7,30 +7,31 @@ beforeEach(() => {
   list = new LinkedList();
   list.append("B");
   list.append("C");
+  list.append("D");
 });
 
 test("Can append a node to the end of the list", () => {
-  list.append("D");
-  expect(list.toString()).toBe("( B ) -> ( C ) -> ( D ) -> null");
+  list.append("E");
+  expect(list.toString()).toBe("( B ) -> ( C ) -> ( D ) -> ( E ) -> null");
 });
 
 test("Can prepend a node to the start of the list", () => {
   list.prepend("A");
-  expect(list.toString()).toBe("( A ) -> ( B ) -> ( C ) -> null");
+  expect(list.toString()).toBe("( A ) -> ( B ) -> ( C ) -> ( D ) -> null");
 });
 
 test("Can get the size of the list", () => {
-  expect(list.size).toBe(2);
+  expect(list.size).toBe(3);
 });
 
 test("Size of the list increases when a node is appended", () => {
   list.append("D");
-  expect(list.size).toBe(3);
+  expect(list.size).toBe(4);
 });
 
 test("Size of the list increases when a node is prepended", () => {
   list.prepend("A");
-  expect(list.size).toBe(3);
+  expect(list.size).toBe(4);
 });
 
 test("Can get the value of the head node in the list,", () => {
@@ -43,12 +44,33 @@ test("Prepended node becomes the new head node,", () => {
 });
 
 test("Can get the value of the tail node in the list,", () => {
-  expect(list.tail()).toBe("C");
+  expect(list.tail()).toBe("D");
 });
 
 test("Appended node becomes the new tail node", () => {
-  list.append("D");
-  expect(list.tail()).toBe("D");
+  list.append("E");
+  expect(list.tail()).toBe("E");
+});
+
+test("Get value at the start of a list", () => {
+  expect(list.at(0)).toBe("B");
+});
+
+test("Get value at the middle of a list", () => {
+  expect(list.at(1)).toBe("C");
+});
+
+test("Get value at the end of a list", () => {
+  expect(list.at(2)).toBe("D");
+});
+
+test("Pop returns the value of the first node", () => {
+  expect(list.pop()).toBe("B");
+});
+
+test("Pop removes the first node from the list ", () => {
+  list.pop();
+  expect(list.toString()).toBe("( C ) -> ( D ) -> null");
 });
 
 //negatives tests
